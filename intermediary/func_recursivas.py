871a2c30d1def4c0156ab4e61f0daf0ef26bef7d
Daniel Brown Rodrigues Mingozzi dos Passos <119956376+DanielBrown1998@users.noreply.github.com>
2024-01-06 18:37:43 -0300

    revisando funções recursivas

--- intermediary/func_recursivas.py
+++ intermediary/func_recursivas.py
@@ -1,3 +1,6 @@
+from typing import List
+from collections import namedtuple
+
 
 # exemplo de uma função não recursiva
 def fatorial(n: int) -> int:
@@ -20,4 +23,26 @@ def fatorial_recursivo(n: int) -> int:
 
 print(int(fatorial_recursivo(5)) == float(fatorial(5)))
 
+box = namedtuple('box', 'have_key')
 
+
+def find_key(boxs: List[box], index: int = 0) -> box:
+    #  base case
+    if len(boxs) <= index:
+        return box(False)
+
+    Box = boxs[index]
+    if Box.have_key:
+        return Box
+
+    index += 1
+    return find_key(boxs, index)
+
+
+if __name__ == '__main__':
+    boxs: List[box] = [
+        box(False), box(False), box(False),
+        box(False), box(False), box(False),
+        box(False), box(True), box(False)
+    ]
+    print(find_key(boxs))

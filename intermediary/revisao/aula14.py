7aad72d355e6a5fd8f5535eba46dace05bf5d027
Daniel Brown Rodrigues Mingozzi dos Passos <119956376+DanielBrown1998@users.noreply.github.com>
2024-02-06 14:37:33 -0300

    estudando map, filter, reduce

--- intermediary/revisao/aula14.py
+++ intermediary/revisao/aula14.py
@@ -20,15 +20,29 @@ def aumentar_preco(p):
     return {**p, 'preço': round(1.5*p['preço'] if p['preço'] < 100 else p['preço'], 2)}
 
 
-novos_precos = map(
-    aumentar_preco,
-    produtos,
+# novos_precos = map(
+#     aumentar_preco,
+#     produtos,
+# )
+
+
+novos_precos = [
+    {**p, 'preço': round(1.5*p['preço'] if p['preço'] < 100 else p['preço'], 2)}  # map
+    for p in produtos
+    if p['preço'] < 100  # filtro
+]
+
+# novos_precos = filter(
+#     lambda p: p['preço'] < 100,
+#     produtos
+# )
+
+total = reduce(
+    lambda accumulated, product: round(accumulated + product['preço'], 2),
+    novos_precos,
+    0.0
 )
-# novos_precos = [
-#     {**p, 'preço': round(1.5*p['preço'] if p['preço'] < 100 else p['preço'], 2)}  # mapeamento
-#     for p in produtos
-# ]
 
-
+print(f"A soma dos preços é {total}")
 print_iter(novos_precos)
 print("tipo: ", type(novos_precos))

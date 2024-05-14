da1a87a1c7aec3dc3139bbffd2515b91af489bb3
Daniel Brown Rodrigues Mingozzi dos Passos <119956376+DanielBrown1998@users.noreply.github.com>
2024-05-14 07:23:17 -0300

    refactor banco_de_dados

--- banco_de_dados/data.py
+++
@@ -1,2 +0,0 @@
-user_sqlite = 'daniel'
-password_sqlite = '1234'
--- intermediary/main.py
+++ intermediary/main.py
@@ -1,2 +1,16 @@
-print(2 ** 2 ** 3)
+def decompose(n):
+    num: int = n**2
+    quad: list = []
+    more: int = 0
+    for c in range(n-1, 0, -1):
+        more += c**2
+        quad.append(c)
+        if more > num:
+            quad.pop()
+            more -= c**2
+    quad.reverse()
+    return quad
 
+
+print(decompose(50))
+print(decompose(8))

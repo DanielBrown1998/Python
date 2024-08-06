b5332698ed7ff26c585657b43cdb74fce2c560ca
Daniel <daniel_academico1998@hotmail.com>
2024-08-06 10:51:34 -0300

    Update main.py
    estudando os

--- _os/main.py
+++ _os/main.py
@@ -1,4 +1,5 @@
 import os
+import shutil
 from pathlib import Path
 import size
 
@@ -37,13 +38,46 @@ caminho_absoluto = os.path.abspath('./visualizacao_dados') # C:\Users\Danie\OneD
 
 #os.walk é uma função que permite percorrer uma estrutura de diretórios de maneira recursiva.
 #os.walk retorna um generator com 3 valores: caminho(root), diretórios, arquivos
-for root, dirs, files in os.walk(caminho_absoluto):
-    if 'my_tdd.py' in files:
-        print(f"Encontrei o arquivo em {root}")
 
-for root, dirs, files in os.walk(caminho_absoluto): 
+#for root, dirs, files in os.walk(caminho_absoluto):
+#    if 'my_tdd.py' in files:
+#        print(f"Encontrei o arquivo em {root}")
+
+#for root, dirs, files in os.walk(caminho_absoluto): 
+#    print(f"{root}-> {size.formata_tamanho(os.path.getsize(root), formatacao=5)}")
+#    for dir_ in dirs:
+#        print(f"=-->{dir_}, {size.formata_tamanho(os.path.getsize(os.path.join(root, dir_)), formatacao=5)}")
+#    for file_ in files:
+#        print(f"=--=-->{file_}, {size.formata_tamanho(os.path.getsize(os.path.join(root, file_)), formatacao=5)}")
+
+
+# os + shutil -> copiar, mover e apagar diretórios
+
+# mover/renomear -> shutil.move / os.rename
+# copiar -> shutil.copy
+# apagar -> os.unlink / shutil.rmtree (recursivamente)
+
+HOME = os.path.expanduser('~') # caminho do usuário
+DIR_TEST = os.path.join(HOME, 'meu_diretorio_teste')
+DIR_TEST_2 = os.path.join(HOME, 'meu_diretorio_teste_2')
+os.makedirs(DIR_TEST_2, exist_ok=True) # criar diretório
+os.makedirs(DIR_TEST, exist_ok=True) # criar um diretório
+
+for root, dirs, files in os.walk(DIR_TEST_2): 
     print(f"{root}-> {size.formata_tamanho(os.path.getsize(root), formatacao=5)}")
     for dir_ in dirs:
-        print(f"=-->{dir_}, {size.formata_tamanho(os.path.getsize(os.path.join(root, dir_)), formatacao=5)}")
-    for file_ in files:
-        print(f"=--=-->{file_}, {size.formata_tamanho(os.path.getsize(os.path.join(root, file_)), formatacao=5)}")
+        caminho_dir = os.path.join(root, dir_)
+        #print(caminho_dir, DIR_TEST)
+        caminho_destino = os.path.join(
+            root.replace(DIR_TEST_2, DIR_TEST), dir_
+        )
+        try:
+            shutil.copy(caminho_dir, caminho_destino)
+        except PermissionError:
+            print(f"Não foi possível copiar {caminho_dir} para {caminho_destino}")
+        except:
+            print('outro erro!')
+        #print(f"=-->{dir_}, {size.formata_tamanho(os.path.getsize(caminho_dir), formatacao=5)}")
+    
+
+#os.rmdir(HOME + '/meu_diretorio_teste') # apagar

ba48e28a455dc8ec64ca9af09f424faab0eb21d5
Daniel <daniel_academico1998@hotmail.com>
2024-08-29 11:38:06 -0300

    estudando set

---
+++ intermediary/revisão/07_set.py
@@ -0,0 +1,18 @@
+conjuntos_even_ = set((c**2, c**3) for c in range(10) if c%2 == 0)
+conjuntos_even = sorted(conjuntos_even_, key=lambda x: x[1])
+# Output: [(0, 0), (4, 8), (16, 64), (36, 216), (64, 512)]
+
+conjuntos_odd_ = set((c**2, c**3) for c in range(10) if c%2 != 0)
+conjuntos_odd = sorted(conjuntos_odd_, key=lambda x: x[1])
+# Output: [(1, 1), (9, 27), (25, 125), (49, 343), (81, 729)]
+
+conjuntos_even_.update(conjuntos_odd_)
+conjuntos_even = sorted(conjuntos_even_, key=lambda x: x[1])
+print(conjuntos_even)
+# Output: [(0, 0), (1, 1), (4, 8), (9, 27), (16, 64), 
+# (25, 125), (36, 216), (49, 343), (64, 512), (81, 729)]
+
+
+
+
+

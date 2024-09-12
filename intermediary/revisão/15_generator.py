4a13218cd7aa1bbedf02b64520204c27c0c5f22a
Daniel <daniel_academico1998@hotmail.com>
2024-09-12 14:42:58 -0300

    estudando generators

--- intermediary/revisão/15_generator.py
+++ intermediary/revisão/15_generator.py
@@ -1,20 +1,38 @@
 #generator são funções que conseguem pausar
-
-def generator(stop, start=0, step=1):
-    if start < stop:
-        while start < stop:
-            yield start
-            start += step
-    else:
-        while start > stop:
-            yield start
-            start -= step
-
-generate = (c for c in generator(10))
-
 #yield pausa a execução do código e volta para a próxima iteração
 #o yield é como um return, mas ao invés de terminar a função, ele
 #pausa a execução e retorna o valor
 #quando a função é chamada novamente, ela continua a partir da próxima iteração
 #o yield pode ser usado para criar iteradores personalizados
 #o yield é uma forma de criar iteradores sem a necessidade de criar uma classe
+
+
+def informative():
+    yield 'executando o Generator: '
+
+def generator(start, stop, step=1):
+
+
+    if start > stop and step > 0 and step != 1:
+        raise ValueError('step must be negative')
+
+    yield from informative()
+
+    if start > stop:
+        step = -1
+        while start > stop:
+            yield start
+            start += step
+    else:
+        while start < stop:
+            yield start
+            start += step
+
+
+gen = generator(10, 0)
+while True:
+    try:
+        print(next(gen), end=' ')
+    except StopIteration:
+        break
+

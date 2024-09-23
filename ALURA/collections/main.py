1eff45b733887281a1792a5a58e145c778ddf580
Daniel <daniel_academico1998@hotmail.com>
2024-09-23 19:04:16 -0300

    estudando collections em python

--- ALURA/collections/main.py
+++ ALURA/collections/main.py
@@ -1,9 +1,25 @@
-import numpy as np
+class ContaCorrente:
+    def __init__(self, codigo):
+        self.codigo = codigo
+        self.saldo = 0
 
-array = ((i, j) for i in range(3) for j in range(3))
+    def deposita(self, valor):
+        self.saldo += valor
 
-while True:
-    try:
-        print(next(array))
-    except StopIteration:
-        break
+    def __str__(self):
+        return f"[>>Codigo {self.codigo} Saldo {self.saldo}<<]"
+
+
+conta_do_gui = ContaCorrente(15)
+conta_do_gui.deposita(500)
+
+conta_da_dani = ContaCorrente(47685)
+conta_da_dani.deposita(1000)
+
+contas = [conta_do_gui, conta_da_dani]
+print(*[conta for conta in contas], sep='\n')
+
+
+contas[1].deposita(2000)
+print(conta_do_gui)
+print(conta_da_dani)
--- intermediary/revisão/modulos/modulo_principal.py
+++ intermediary/revisão/modulos/modulo_principal.py
@@ -1,2 +1,3 @@
 
+
 print(f'Ao importar o módulo {__name__}, o código aqui será executado')

f6f79af2d27a701028757a829e7b68d06ff9d2d5
Daniel <daniel_academico1998@hotmail.com>
2024-10-10 16:21:30 -0300

    "Refactor decorator implementation to use a factory function with a list of allowed values, and update the decorated function to accept a list of integers and return a sorted list."

--- intermediary/revisão/19_decorators.py
+++ intermediary/revisão/19_decorators.py
@@ -1,22 +1,31 @@
+from typing import List
+
+def decorator_factory(values: List[int]):    
+    def factory_function(func):
+        for item in values:
+            if not isinstance(item, (int)):
+                raise ValueError("Only lists of integers are allowed")
+        def inner_function(*args, **kwargs):
+            nonlocal values
+            values_temp = []
+            for item in args:
+                if not isinstance(item, (int)):
+                    del values_temp
+                    raise ValueError("Only params of integers are allowed")
+                if item not in values:
+                    values_temp.append(item)
+            values += values_temp
+            print("Before calling the function")
+            return func(*args, lista = values)
+        return inner_function
+
+    return factory_function
+
+lista = [c for c in range(10)]
+@decorator_factory(lista)
+def add_lista(*args, lista:List[int] | None = None) -> List[int]:
+    return sorted(lista)
 
 
-def decorator(func):
-    def intern(*args):
-        for item in args:
-            if not isinstance(item, (int, float)):
-                raise ValueError("Only integers or floats are allowed")
-        print("Before calling the function")
-        return func(*args)
-    return intern
-
-@decorator
-def soma(a, b):
-    print(f'I`ll sum this numbers: {a}, {b}')
-    return a + b
-
-
-print(soma(1, 2))
-
-
-
-
+lista = add_lista(1, 52, 33, 4, 15, 6, 12, 13, 51, lista=lista)
+print(lista)

bca61f0d3b307514a6489eb37c415a3887cc6c2b
Daniel <daniel_academico1998@hotmail.com>
2024-10-10 17:12:42 -0300

    Atualizações de código para corrigir bugs e melhorar o desempenho.

---
+++ intermediary/revisão/20_exercicio_listas.py
@@ -0,0 +1,8 @@
+capitais = ['Salvador', 'Ubatuba', 'Belo Horizonte', 'Rio de Janeiro']
+estados = ['BA', 'SP', 'MG', 'RJ']
+
+estados_x_capitais = zip(
+    estados,
+    capitais
+)
+print(list(estados_x_capitais))

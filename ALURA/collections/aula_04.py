9e1aad4e77677f2791a008738c57bb364dcc4151
Daniel <daniel_academico1998@hotmail.com>
2024-10-15 15:00:25 -0300

    estudando collections

--- ALURA/collections/aula_04.py
+++ ALURA/collections/aula_04.py
@@ -1,40 +1,48 @@
 class ContaSalario:
     def __init__(self, codigo) -> None:
-        self._codigo = codigo
+        self.codigo = codigo
         self._saldo = 0
 
     def deposita(self, valor) -> None:
         self._saldo += valor
 
     def __str__(self) -> str:
-        return f'[>>Codigo {self._codigo} Saldo {self._saldo}<<]'
+        return f'[>>Codigo {self.codigo} Saldo {self._saldo}<<]'
 
     def __eq__(self, outro) -> bool:
         if type(outro) != ContaSalario:
             return False
 
-        return self._codigo == outro._codigo and self._saldo == outro._saldo
+        return self.codigo == outro.codigo and self._saldo == outro._saldo
 
     def __lt__(self, outro) -> bool:
         if type(outro) != ContaSalario:
             raise TypeError(f"{type(outro)} não é do tipo {ContaSalario}")
-        if self._codigo != outro._codigo:
-            raise KeyError(f"Os códigos das contas não são condizentes!!!")
+        
+        diferenca_saldo = self._saldo - outro._saldo
+
+        if diferenca_saldo == 0:
+            return self.codigo < outro.codigo
+
         return self._saldo < outro._saldo
     
     def __le__(self, outro) -> bool:
         if type(outro) != ContaSalario:
             raise TypeError(f"{type(outro)} não é do tipo {ContaSalario}")
-        if self._codigo != outro._codigo:
-            raise KeyError(f"Os códigos das contas não são condizentes!!!")
+        
+        diferenca_saldo = self._saldo - outro._saldo
+
+        if diferenca_saldo == 0:
+            return self.codigo <= outro.codigo
+
         return self._saldo <= outro._saldo
 
 
 conta1 = ContaSalario(37)
 conta1.deposita(100)
-conta2 = ContaSalario(37)
-conta2.deposita(1020)
-print(conta1 == conta2)
-print(conta1 <= conta2)
-print(conta1 < conta2)
-print(conta1 > conta2)
+conta2 = ContaSalario(23)
+conta2.deposita(1010)
+contas = [conta1, conta2]
+contas = sorted(contas)
+for conta in contas:
+    print(conta)

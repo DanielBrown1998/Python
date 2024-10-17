dc249275564449334b40805d1c1868e105834964
Daniel <daniel_academico1998@hotmail.com>
2024-10-17 14:20:52 -0300

    unindo listas

--- intermediary/revisão/20_exercicio_listas.py
+++ intermediary/revisão/20_exercicio_listas.py
@@ -1,8 +1,23 @@
+from typing import List, Generator
 capitais = ['Salvador', 'Ubatuba', 'Belo Horizonte', 'Rio de Janeiro']
 estados = ['BA', 'SP', 'MG', 'RJ']
+
+
+def zipper(lista_a: List,lista_b: List) -> Generator:
+    TAM = min(
+        (len(lista_a), len(lista_b))
+        )
+    return (
+        (lista_a[i], lista_b[i]) for i in range(TAM)
+    )
+
+
 
 estados_x_capitais = zip(
     estados,
     capitais
 )
+
+print(list(zipper(capitais, estados)))
+
 print(list(estados_x_capitais))

52b6fdf63cd569a335c037096b79badb0eddce83
Daniel <daniel_academico1998@hotmail.com>
2025-01-15 10:41:07 -0300

    Adiciona função para calcular e visualizar a rota mais curta entre dois pontos utilizando matplotlib

--- ALURA/otimizacao/main.py
+++ ALURA/otimizacao/main.py
@@ -1,27 +1,71 @@
 import matplotlib.pyplot as plt
 from scipy.spatial.distance import euclidean as distance_euclidean
+from typing import List, Tuple
+from itertools import permutations
 
-origem = [(0, 0)] 
-destino = [(4, 4)]
-distancia_percorrida = 0 
+
+def desenhar_rota(origem: Tuple[int], destino: Tuple[int], enderecos: List[Tuple[int]], show=False):
+    #validacao de origme e destino
+    if len(origem) != 2:
+        raise ValueError(f"{origem} não tem 2 argumentos")
+
+    elif len(destino) != 2:
+        raise ValueError(f"{destino} não tem 2 argumentos")
+    
+    #validação de enderecos
+    for ponto in enderecos:
+        if len(ponto) != 2:
+            raise AttributeError(f" O ponto {ponto} da posição {e} de enderecos não possui 2 valores")
+        
+    #setando a distancia percorrida a partir de enderecos
+    distancia_percorrida = 0
+    for e in range(len(enderecos)):
+        if e < len(enderecos) -1:
+            distancia_percorrida += distance_euclidean(enderecos[e], enderecos[e+1])
+
+    # percorrendo as possíveis rotas para determinar a menor possível entre origem e destino
+    for rota in permutations(enderecos):
+        distancia_provisoria = 0
+        rota_tam = len(rota)
+        for e in range(rota_tam):
+            if e < rota_tam -1:
+                distancia_provisoria += distance_euclidean(rota[e], rota[e+1])
+        #setando a menor distancia entre origem e destino
+        if distancia_provisoria < distancia_percorrida:
+            distancia_percorrida = distancia_provisoria 
+            enderecos = rota
+    #resetando distancia_percorrida e determinando rota    
+    distancia_percorrida = 0
+    rota = [tuple(origem)] + [c for c in enderecos] + [tuple(destino)]
+    print(rota)
+    #construindo o gráfico
+    for e, ponto in enumerate(rota):
+        x, y = ponto
+        if show:
+            cor = "black"
+            if e == 0:
+                cor = "blue"
+            elif e == len(rota) - 1:
+                cor = "red"
+            plt.scatter(x, y, color=cor)
+    
+        if e < len(rota)-1:
+            x1, y1 = rota[e+1]
+            distancia_percorrida += distance_euclidean(rota[e], rota[e+1])
+            if show:
+                dx = x1 - x
+                dy = y1 - y
+                plt.arrow(x, y, dx, dy, color="black", head_width=.1)
+    if show:
+        plt.title(f"distância da rota: {round(distancia_percorrida, 2)}")
+        plt.show()
+
+    return distancia_percorrida, rota
+
 enderecos = [(2, 1), (2, 2), (8, 3), (5, 3)]
 
-rota = origem + enderecos + destino
+origem = 0, 0
+destino = 4, 4
 
-for e, ponto in enumerate(rota):
-    x, y = ponto
-    cor = "black"
-    if e == 0:
-        cor = "blue"
-    elif e == len(rota) - 1:
-        cor = "red"
-    plt.scatter(x, y, color=cor)
-    if e < len(rota)-1:
-        x1, y1 = rota[e+1]
-        distancia_percorrida += distance_euclidean(rota[e], rota[e+1])
-    dx = x1 - x
-    dy = y1 - y
-    plt.arrow(x, y, dx, dy, color="black", head_width=.1)
-
-plt.title(f"distância da rota: {round(distancia_percorrida, 2)}")
-plt.show()
+tam, rota = desenhar_rota(origem, destino, enderecos, show=True)
+print(tam, rota)

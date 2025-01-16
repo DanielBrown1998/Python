1a042651351a5388c0e49df18d0ce440071d2079
Daniel <daniel_academico1998@hotmail.com>
2025-01-16 18:57:47 -0300

    Refatora implementação do algoritmo genético para modularizar a lógica e facilitar a reutilização

--- ALURA/otimizacao/algoritmo_genetico.py
+++ ALURA/otimizacao/algoritmo_genetico.py
@@ -16,38 +16,44 @@ def avaliacao(individuo: List[int], origem: Tuple[int], enderecos: List[Tuple[in
 
     return distancia_percorrida, 
 
+def algoritmos_geneticos(origem: Tuple[int], enderecos: List[Tuple[int]], destino: Tuple[int], tam_populacao_inicial: int, prob_cruzamento: float, prob_mutacao: float, num_geracoes: int) -> Tuple:
 
-tam_populacao_inicial = 100
-prob_cruzamento =.7
-prob_mutacao = .1
-numero_geracoes = 100
+    creator.create("FitnessMin", base.Fitness, weights=(-1.0,))
+    creator.create("Individuo", list, fitness=creator.FitnessMin)
 
-creator.create("FitnessMin", base.Fitness, weights=(-1.0,))
-creator.create("Individuo", list, fitness=creator.FitnessMin)
+    toolbox = base.Toolbox()
+    toolbox.register("Genes", np.random.permutation, len(enderecos))
+    toolbox.register("Individuos", tools.initIterate, creator.Individuo, toolbox.Genes)
 
-toolbox = base.Toolbox()
-toolbox.register("Genes", np.random.permutation, len(enderecos))
-toolbox.register("Individuos", tools.initIterate, creator.Individuo, toolbox.Genes)
+    toolbox.register("Populacao", tools.initRepeat, list, toolbox.Individuos)
+    populacao = toolbox.Populacao(n=tam_populacao_inicial)
+        
+    toolbox.register("mate", tools.cxPartialyMatched)
+    toolbox.register("mutate", tools.mutShuffleIndexes, indpb=prob_mutacao)
+    toolbox.register("select", tools.selTournament, tournsize=3)
+    toolbox.register("evaluate", avaliacao, origem=origem, enderecos=enderecos, destino=destino)
 
-toolbox.register("Populacao", tools.initRepeat, list, toolbox.Individuos)
-populacao = toolbox.Populacao(n=tam_populacao_inicial)
-    
-toolbox.register("mate", tools.cxPartialyMatched)
-toolbox.register("mutate", tools.mutShuffleIndexes, indpb=prob_mutacao)
-toolbox.register("select", tools.selTournament, tournsize=3)
-toolbox.register("evaluate", avaliacao, origem=origem, enderecos=enderecos, destino=destino)
+    algoritmo = algorithms.eaSimple(
+        populacao,
+        toolbox, 
+        cxpb=prob_cruzamento,
+        mutpb=prob_cruzamento,
+        ngen=numero_geracoes, 
+        verbose=False
+    )
 
-algoritmo = algorithms.eaSimple(
-    populacao,
-    toolbox, 
-    cxpb=prob_cruzamento,
-    mutpb=prob_cruzamento,
-    ngen=numero_geracoes, 
-    verbose=False
-)
+    melhor_ind = tools.selBest(populacao, 1)[0]
+    enderecos = [enderecos[i] for i in melhor_ind]
+    melhor_rota = [origem]+ enderecos +[destino]
 
+    return melhor_rota, avaliacao(melhor_ind, origem, enderecos, destino) 
 
-melhor_ind = tools.selBest(populacao, 1)[0]
-enderecos = [enderecos[i] for i in melhor_ind]
-melhor_rota = [origem]+ enderecos +[destino]
-print(melhor_rota, avaliacao(melhor_ind, origem, enderecos, destino))
+if __name__ == "__main__":
+    tam_populacao_inicial = 100
+    prob_cruzamento =.7
+    prob_mutacao = .1
+    numero_geracoes = 100
+
+    melhor_rota, distancia = algoritmos_geneticos(origem, enderecos, destino, tam_populacao_inicial, prob_cruzamento, prob_mutacao, numero_geracoes)
+
+    print(melhor_rota, distancia)
